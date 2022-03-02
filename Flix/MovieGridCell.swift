39dcfc396488654fde716dfef85dbaf7f4814aa8
Praggathi Raja Rao <praggathi.rr@gmail.com>
2022-03-02 01:14:03 -0800

    Part 2
    Navigation bar

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Praggathi Raja Rao on 3/2/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
